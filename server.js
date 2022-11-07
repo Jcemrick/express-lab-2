e642cc33de9c9c21daf03898890f197c79129b12
Joe Emrick <jcemrick@gmail.com>
2022-11-07 12:23:39 -0800

    tip calculator express application created

--- server.js
+++ server.js
@@ -18,8 +18,18 @@ const PORT = process.env.PORT || 3002;
 // ------------------
 // Declare routes and routers
 // ------------------
+
+// Greeting section
 app.get('/greeting/:name', (req, res) =>{
   res.send(`Hello, Bob...wait a minute, you're not Bob! You're ${req.params.name}!`)
+})
+
+
+// Tip Calculator Section
+app.get('/tip/:total/:percentage', (req, res) => {
+  let percentage = parseInt(req.params.percentage) / 100;
+  let tip = parseFloat(parseInt(req.params.total) * percentage).toFixed(2);
+  res.send(`Your tip should be $${tip}`)
 })
 
 
